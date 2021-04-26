19c184e897a22e233b5741f6d938ac604516d498
Akram Sabbah <akramsabbah9@gmail.com>
2021-04-25 19:42:21 -0700

    minor formatting

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,16 +3,16 @@ import React from "react";
 
 function About() {
     return (
-        <div>{/* TODO: add flex class */}
+        <div>{/*TODO: flex*/}
             <img src={require("../../assets/me.jpg").default} alt="Akram Sabbah" />
-            <div>
-            <h2>Who Am I?</h2>
-            <p>
-                I'm a recently-graduated software engineer from UCLA, based in Pleasonton, CA. I specialize in full-stack web development 
-                and embedded systems programming. My passion is creating intuitive user-friendly applications 
-                with an emphasis on mobile-first paradigms. Feel free to contact me if you have any questions 
-                about <span>my portfolio</span> or otherwise.
-            </p>
+            <div>{/*TODO: no flex*/}
+                <h2>Who Am I?</h2>
+                <p>
+                    I'm a recently-graduated software engineer from UCLA, based in Pleasonton, CA. I specialize in full-stack web development 
+                    and embedded systems programming. My passion is creating intuitive user-friendly applications 
+                    with an emphasis on mobile-first paradigms. Feel free to contact me if you have any questions 
+                    about <span>my portfolio</span> or otherwise.
+                </p>
             </div>
         </div>
     )
