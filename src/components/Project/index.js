493552de74214ea5ed6ae6007e03d22d90ec8496
Akram Sabbah <akramsabbah9@gmail.com>
2021-04-26 19:43:23 -0700

    retooled project cards

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,19 +1,23 @@
 /* Project: display a single project card. */
-import React, { useState } from "react";
+import React from "react";
 
 function Project(props) {
     const { name, github, deployed } = props;
     return (
-        <div className="card-container">
-            <img
-                //src={require(`../../assets/small/${category}/${i}.jpg`).default}
-                alt={name}
-                //onClick={() => toggleDisplay(name)}
-                key={name}
-            />
-            <a href={deployed} target="_blank" rel="noreferrer noopener">Try it Here!</a> {/*link*/}
-            <a href={github} target="_blank" rel="noreferrer noopener">GitHub</a> {/*github*/}
-        </div>
+        <article className="card">
+            <div className="card-header">
+                <img
+                    //src={require(`../../assets/small/${category}/${i}.jpg`).default}
+                    alt={name}
+                    //onClick={() => toggleDisplay(name)}
+                    key={name}
+                />
+            </div>
+            <div className="card-footer">
+                <a href={deployed} target="_blank" rel="noreferrer noopener">Try it Here!</a> {/*link*/}
+                <a href={github} target="_blank" rel="noreferrer noopener">GitHub</a> {/*github*/}
+            </div>
+        </article>
     );
 }
 
