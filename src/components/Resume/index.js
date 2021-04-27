2ff411d6dcee331036d75a2f7af0e9b51ad3216e
Akram Sabbah <akramsabbah9@gmail.com>
2021-04-27 02:21:36 -0700

    render frontend and backend skills

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,11 +1,26 @@
 /* Resume: link to resume and list coding proficiencies. */
-import React from "react";
+import React, { useState } from "react";
 import MyResume from "../../assets/docs/resume.pdf";
 
 function Resume() {
+    const [skills] = useState({
+        frontend: ["Javascript", "HTML", "CSS", "React", "jQuery", "Bootstrap", "Bulma", "Handlebars"],
+        backend: ["Python", "Node.js", "Express", "Django", "MySQL", "Sequelize", "MongoDB", "Mongoose", "REST APIs", "Jest"]
+    });
+
     return (
         <>
             <p>View my resume <a href={MyResume} target="_blank" rel="noreferrer noopener">here</a>.</p>
+
+            <h2>Front-End Skills</h2>
+            <ul>
+                {skills.frontend.map(skill => <li>{skill}</li>)}
+            </ul>
+
+            <h2>Back-End Skills</h2>
+            <ul>
+                {skills.backend.map(skill => <li>{skill}</li>)}
+            </ul>
         </>
     );
 }
