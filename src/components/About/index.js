3a7329d1e26eb8f8b5a873217047b0434c834a61
Akram Sabbah <akramsabbah9@gmail.com>
2021-04-28 14:43:01 -0700

    overhauled About style

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,21 +1,31 @@
 /* About: an "about me" section featuring picture and bio */
 import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
 
 function About() {
     return (
-        <div>{/*TODO: flex*/}
-            <img src={require("../../assets/img/me.jpg").default} alt="Akram Sabbah" />
-            <div>{/*TODO: no flex*/}
-                <h2>Who Am I?</h2>
+        <Row>
+            <Col className="d-flex justify-content-center" md="6">
+                <Image
+                    src={require("../../assets/img/me.jpg").default}
+                    className="h-75 mt-3"
+                    alt="Akram Sabbah"
+                    rounded fluid
+                />
+            </Col>
+            <Col md>
+                <h2 className="h3 mt-2">Who Am I?</h2>
                 <p>
-                    I'm a recently-graduated software engineer from UCLA, based in Pleasonton, CA. I specialize in full-stack web development 
-                    and embedded systems programming. My passion is creating intuitive user-friendly applications 
-                    with an emphasis on mobile-first paradigms. Feel free to contact me if you have any questions 
-                    about <span>my portfolio</span> or otherwise.
+                    I'm a recently-graduated software engineer from UCLA, based in Pleasonton, CA. I specialize 
+                    in full-stack web development and embedded systems programming. My passion is creating intuitive 
+                    user-friendly applications with an emphasis on mobile-first paradigms. Feel free to contact me 
+                    if you have any questions about <span>my portfolio</span> or otherwise.
                 </p>
-            </div>
-        </div>
-    )
+            </Col>
+        </Row>
+    );
 }
 
 export default About;
