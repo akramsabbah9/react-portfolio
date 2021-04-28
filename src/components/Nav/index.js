b67d72527814444b23533c1a0384ce53653cbbb0
Akram Sabbah <akramsabbah9@gmail.com>
2021-04-28 02:55:55 -0700

    bold nav section that is currently displayed

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -6,9 +6,12 @@ function MNav(props) {
     const {sections = [], current, setCurrent} = props;
 
     return (
-        <Nav className="w-100 pt-3 pb-3 justify-content-around text-white col-lg-9 row">
+        <Nav className="w-100 pt-3 pb-3 justify-content-around text-light col-lg-9 row">
             {sections.map(section => (
-                <Nav.Item key={section.name} className="col-sm text-center">
+                <Nav.Item
+                    key={section.name}
+                    className={`col-sm text-center ${current.name === section.name && "font-weight-bolder text-white"}`}
+                >
                     <span onClick={() => setCurrent(section)}>{section.name}</span>
                 </Nav.Item>
             ))}
