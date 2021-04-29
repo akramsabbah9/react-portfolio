1b0446b132aa2755a63c50784316b91fca4fdae8
Akram Sabbah <akramsabbah9@gmail.com>
2021-04-28 18:10:10 -0700

    styled resume page

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,26 +1,40 @@
 /* Resume: link to resume and list coding proficiencies. */
 import React, { useState } from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import MyResume from "../../assets/docs/resume.pdf";
 
 function Resume() {
     const [skills] = useState({
         frontend: ["Javascript", "HTML", "CSS", "React", "jQuery", "Bootstrap", "Bulma", "Handlebars"],
-        backend: ["Python", "Node.js", "Express", "Django", "MySQL", "Sequelize", "MongoDB", "Mongoose", "REST APIs", "Jest"]
+        backend: ["Python", "Node.js", "Express", "Django", "MySQL / Sequelize", "MongoDB / Mongoose", "REST APIs", "Jest"]
     });
 
     return (
         <>
-            <p>View my resume <a href={MyResume} target="_blank" rel="noreferrer noopener">here</a>.</p>
+            <div class="text-center">
+                <p>View my resume <a href={MyResume} target="_blank" rel="noreferrer noopener">here</a>.</p>
+            </div>
 
-            <h2>Front-End Skills</h2>
-            <ul>
-                {skills.frontend.map(skill => <li>{skill}</li>)}
-            </ul>
+            <Row>
+                <Col className="d-flex justify-content-center" xs>
+                <div>
+                <h2 className="h3">Front-End Skills</h2>
+                <ul>
+                    {skills.frontend.map(skill => <li key={skill}>{skill}</li>)}
+                </ul>
+                </div>
+                </Col>
 
-            <h2>Back-End Skills</h2>
-            <ul>
-                {skills.backend.map(skill => <li>{skill}</li>)}
-            </ul>
+                <Col className="d-flex justify-content-center" xs>
+                <div>
+                <h2 className="h3">Back-End Skills</h2>
+                <ul>
+                    {skills.backend.map(skill => <li key={skill}>{skill}</li>)}
+                </ul>
+                </div>
+                </Col>
+            </Row>
         </>
     );
 }
