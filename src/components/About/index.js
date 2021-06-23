6bf91eaa75d563f931377fe28700a480f86cd9be
Akram Sabbah <akramsabbah9@gmail.com>
2021-06-23 13:39:26 -0700

    fixed About typo

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -21,7 +21,7 @@ function About() {
             <Col md>
                 <h2 className="h3 mt-2">Who Am I?</h2>
                 <p>
-                    I'm a recently-graduated software engineer from UCLA, based in Pleasonton, CA. I specialize 
+                    I'm a recently-graduated software engineer from UCLA, based in Pleasanton, CA. I specialize 
                     in full-stack web development and embedded systems programming. My passion is creating intuitive 
                     user-friendly applications with an emphasis on mobile-first paradigms. Feel free to contact me 
                     if you have any questions about <span>my portfolio</span> or otherwise.
